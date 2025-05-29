c12f49fdbe9f37877e6c704d83c1b684caf79c5b
Bharath Vaji <bharathvaji57@gmail.com>
2025-05-29 13:00:38 +0530

    new file added

--- test.py
+++
@@ -1,9 +0,0 @@
-import util  # your util.py file
-
-util.load_saved_artifacts()
-
-print(util.__data_columns)  # print columns loaded
-
-# Test prediction with sample input:
-price = util.predict_price('1st Phase JP Nagar', 1000, 2, 2)
-print(f"Predicted price for sample input: {price}")
